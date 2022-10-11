bf0e07f29d32c358d859b479400c417407378052
afterth3n <afterth3n@gmail.com>
2022-10-10 19:56:06 -0700

    10/10/2022
    added more to course 1 finished moving platform

--- Placeholder/Assets/PlatformMover.cs
+++ Placeholder/Assets/PlatformMover.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlatformMover : MonoBehaviour
 {
-    Vector3 pos1 = new Vector3(-f, f, f);
-    Vector3 pos2 = new Vector3(-118f, 72.5f, 70f);
+    Vector3 pos1 = new Vector3(-115f, 72.5f, 92f);
+    Vector3 pos2 = new Vector3(-115f, 72.5f, 70f);
     bool check = false;
     // Start is called before the first frame update
     void Start()
@@ -18,11 +18,28 @@ public class PlatformMover : MonoBehaviour
     {
         if(!check)
         {
-            transform.position = Vector3.Lerp(pos2, pos1, 5f);
+            transform.position = Vector3.MoveTowards(transform.position, pos1, 12f * Time.deltaTime);
             if(Vector3.Distance(transform.position, pos1) < 1)
             {
                 check = true;
             }
         }
+        if(check)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, pos2, 12f * Time.deltaTime);
+            if(Vector3.Distance(transform.position, pos2) < 1)
+            {
+                check = false;
+            }
+        }
+    }
+    
+    void OnCollisionEnter(Collision other)
+    {
+        other.gameObject.transform.SetParent(gameObject.transform, true);
+    }
+    void OnCollisionExit(Collision other)
+    {
+        other.gameObject.transform.parent = null;
     }
 }
